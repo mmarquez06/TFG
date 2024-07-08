e705f1f36ed89a85110ca0f71e82abca26e5bc62
Maria Marquez Luquin <mluquin2002@icloud.com>
2024-07-08 16:48:59 +0200

    Add files via upload

---
+++ Codigos Arduino/Acelerometro.ino
@@ -0,0 +1,147 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+double counter;
+
+double media_acc_x;
+double media_acc_y;
+double media_acc_z;
+
+double acc_x;
+double acc_y;
+double acc_z;
+
+double cal_x = 0;
+double cal_y = 0;
+double cal_z = 0;
+
+double gradosejex;
+double gradosejey;
+double gradosejez;
+
+int umbral = 45;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+}
+
+void loop() {
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  //Las primeras 20 lecturas sirven para calibrar el sensor
+ if (counter<20){
+
+   // Calibrar el acelerometro a 1g en el eje z (ajustar el offset)
+   media_acc_x = media_acc_x + a.acceleration.x;
+
+   counter++;
+  }
+  else{
+   cal_x = media_acc_x/20;
+
+  }
+
+      acc_x = a.acceleration.x ;
+      acc_y = a.acceleration.y ;
+      acc_z = a.acceleration.z ;
+
+    gradosejex = atan(acc_x/sqrt((pow(2,acc_y)+(pow(2,acc_z)))))*100-cal_x;
+
+    if (gradosejex<umbral){
+      Serial.print("CUIDAO SA CAIO");
+       Serial.print(gradosejex);
+       Serial.println("grados");
+       
+
+    }
+
+    else {
+      Serial.print("grados");
+      Serial.println(gradosejex);
+    }
+
+  
+
+  Serial.println("");
+  delay(500);
+}
